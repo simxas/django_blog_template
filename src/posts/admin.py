15b271305872d1c1e961eaf7c84b154029b7633a
Simon Lickus <darbolickus@gmail.com>
2017-01-21 21:25:22 +0200

    posts admin

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from embed_video.admin import AdminVideoMixin
 
-# Register your models here.
+from .models import Post, Category, CategoryToPost
+
+class CategoryToPostInline(admin.TabularInline):
+    model = CategoryToPost
+    extra = 1
+
+class PostModelAdmin(AdminVideoMixin, admin.ModelAdmin):
+    list_display = ["title", "timestamp", "updated"]
+    list_display_links = ["timestamp", "updated"]
+    list_editable = ["title"]
+    list_filter = ["updated", "timestamp"]
+    search_fields = ["title", "content"]
+    inlines = [CategoryToPostInline]
+    class Meta:
+        model = Post
+
+class CategoryModelAdmin(AdminVideoMixin, admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ["title"]
+    class Meta:
+        model = Category
+
+admin.site.register(Post, PostModelAdmin)
+admin.site.register(Category, CategoryModelAdmin)
