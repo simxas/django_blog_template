3e3b85a7275f52a745e185ebbe941f77735f479e
Simon Lickus <darbolickus@gmail.com>
2017-01-21 21:44:45 +0200

    urls, views, templates

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+# Posts list view
+def posts_list(request):
+    context = {
+        "title": "posts list",
+    }
+    return render(request, "posts_list.html", context)
+
+# Post detail view
+def post_detail(request, slug):
+    context = {
+        "title": "post detail",
+    }
+    return render(request, "post_detail.html", context)
+
+# Posts category view
+def post_category(request, slug):
+    context = {
+        "title": "post category",
+    }
+    return render(request, "post_category.html", context)
+
+# Create Post view
+def post_create(request):
+    context = {
+        "title": "post create",
+    }
+    return render(request, "post_form.html", context)
+
+# Update Post view
+def post_update(request, slug):
+    context = {
+        "title": "update post",
+    }
+    return render(request, "post_form.html", context)
+
+# Delete Post view
+def post_delete(request, slug=None):
+    context = {
+        "title": "delete post",
+    }
+    return redirect("posts:list")
